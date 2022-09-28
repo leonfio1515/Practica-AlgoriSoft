de2896fb95d0127e9814b7116262a63ad0369654
leonfio1515 <leonfio1515@gmail.com>
2022-09-28 17:57:31 -0300

    ajax

--- django-3/web/MiApp/views.py
+++ django-3/web/MiApp/views.py
@@ -1,6 +1,10 @@
 from msilib.schema import ListView
 from django.views.generic import TemplateView, ListView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 from .models import *
+from django.http import JsonResponse
 # Create your views here.
 
 
@@ -11,6 +15,14 @@ class InicioView(TemplateView):
 class CategoryListView(ListView):
     model = Category
     template_name = 'category/category_list.html'
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwars):
+        return super().dispatch(request, *args, **kwars)
+
+    def post(self, request, *args, **kwargs):
+        data = {"name":"Leonfio"}
+        return JsonResponse(data)
 
 # Controla el QuerySet del modelo, y podemos determinar el filtro a aplicar
     def get_queryset(self):
